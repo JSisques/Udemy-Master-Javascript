5d55f0085a81a458ceda35b12c17818ab9279336
Javi Plaza <javierplazasisques@gmail.com>
2021-10-13 17:59:09 +0200

    Seccion 7: Operadores y tipos de datos

---
+++ JavaScript/Seccion 7: Tipos de datos y operadores/js/main.js
@@ -0,0 +1,14 @@
+'use-strict'
+
+var entero = 1;
+var float = 1.5;
+
+//Operadores
+var suma = entero + float;
+var resta = entero - float;
+var multi = entero * float;
+var division = entero / float;
+var modulo = entero % float;
+
+//Tipos de datos
+var string = "Hola";
