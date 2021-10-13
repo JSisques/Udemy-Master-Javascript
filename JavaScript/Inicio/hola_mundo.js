27281c82c780f9606eee2cd8d5e57c3151297ab7
Javi Plaza <javierplazasisques@gmail.com>
2021-10-13 17:35:45 +0200

    Seccion 5: La consola del navegador

--- JavaScript/Inicio/hola_mundo.js
+++ JavaScript/Inicio/hola_mundo.js
@@ -1,3 +1,5 @@
 alert("Hola mundo con JS");
 alert("Bienvenido al curso");
-document.write("Hola mundo fichero externo")
+document.write("Hola mundo fichero externo");
+console.log("Muestra esto en la consola");
+console.log(88 + 4);
