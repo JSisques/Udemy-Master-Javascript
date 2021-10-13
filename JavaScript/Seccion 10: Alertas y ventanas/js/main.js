823bed8d339533e5fd3ddd3b4f2288723a994087
Javi Plaza <javierplazasisques@gmail.com>
2021-10-13 18:22:56 +0200

    Seccion 10: Alertas y ventanas

---
+++ JavaScript/Seccion 10: Alertas y ventanas/js/main.js
@@ -0,0 +1,9 @@
+alert("Esto es una alerta");
+
+//Confirmación
+var result = confirm("Ventana de confirmación");
+console.log(result);
+
+//Ingreso datos
+result = prompt("Escribe tu edad", 18);
+console.log(result);
