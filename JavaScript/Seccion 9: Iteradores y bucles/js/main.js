59de90c2fdbc9e9d904f061fb0d0499fa4860cc6
Javi Plaza <javierplazasisques@gmail.com>
2021-10-13 18:18:01 +0200

    Seccion 9: Bucle While

--- JavaScript/Seccion 9: Iteradores y bucles/js/main.js
+++ JavaScript/Seccion 9: Iteradores y bucles/js/main.js
@@ -1,8 +1,14 @@
 'use-strict'
 
 var numero = 100;
+var contador = 0;
 
 for (let index = 0; index < numero; index++) {
     console.log(index);
     
+}
+
+while(contador != 100){
+    console.log(contador);
+    contador++;
 }
