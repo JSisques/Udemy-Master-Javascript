c950a0cb46d2ff5029fe08f2932509fd9197a064
Javi Plaza <javierplazasisques@gmail.com>
2021-10-13 18:39:55 +0200

    Seccion 11: Ejercicio 2

--- JavaScript/Seccion 11: Ejercicios de programacion/js/main.js
+++ JavaScript/Seccion 11: Ejercicios de programacion/js/main.js
@@ -3,7 +3,6 @@
 Ejercicio 1:
 Programa que pida dos numeros y que nos diga cual es el menor, el mayor o si son iguales.
 Si los numeros son menores o iguales a cero nos tiene que volver a pedir el numero
-*/
 
 do {
     var num1 = prompt("Introduce el numero 1");
@@ -17,19 +16,55 @@ if(num1 > num2){
 } else{
     alert(num1 + " es igual que " + num2);
 }
+*/
 
 /*
-Ejercicio 1:
+Ejercicio 2:
+Utilizando un bucle pedir numeros y calcular su suma y su media hasta introducir un numero negativo
+*/
+var contador = 0;
+var num = 0;
+var suma = 0;
+var media = 0   
+do {
+    
+    suma += num;
+    contador++;
+    media = suma / contador;
+
+    console.log(suma);
+    console.log(media);
+
+    num =  parseInt(prompt("Introduce un número"));
+
+} while (num >= 0);
+
+/*
+Ejercicio 3:
 Programa que pida dos numeros y que nos diga cual es el menor, el mayor o si son iguales.
 */
 
-var num1 = prompt("Introduce el numero 1");
-var num2 = prompt("Introduce el numero 2");
+/*
+Ejercicio 4:
+Programa que pida dos numeros y que nos diga cual es el menor, el mayor o si son iguales.
+*/
 
-if(num1 > num2){
-    alert(num1 + " es mayor que " + num2);
-} else if(num1 < num2){
-    alert(num1 + " es menor que " + num2);
-} else{
-    alert(num1 + " es igual que " + num2);
-}
+/*
+Ejercicio 5:
+Programa que pida dos numeros y que nos diga cual es el menor, el mayor o si son iguales.
+*/
+
+/*
+Ejercicio 6:
+Programa que pida dos numeros y que nos diga cual es el menor, el mayor o si son iguales.
+*/
+
+/*
+Ejercicio 7:
+Programa que pida dos numeros y que nos diga cual es el menor, el mayor o si son iguales.
+*/
+
+/*
+Ejercicio 8:
+Programa que pida dos numeros y que nos diga cual es el menor, el mayor o si son iguales.
+*/
