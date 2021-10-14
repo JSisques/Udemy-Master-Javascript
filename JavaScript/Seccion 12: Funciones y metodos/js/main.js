3d774b387384fd38cc5ce8a9f3b9fff03fa90457
Javi Plaza <javierplazasisques@gmail.com>
2021-10-14 12:09:26 +0200

    Seccion 12: Plantillas de texto

--- JavaScript/Seccion 12: Funciones y metodos/js/main.js
+++ JavaScript/Seccion 12: Funciones y metodos/js/main.js
@@ -113,3 +113,9 @@ var busqueda = texto1.split(" ")
 console.log(busqueda);
 var busqueda = texto1.trim()
 console.log(busqueda);
+
+//Plantillas de texto
+var nombre = prompt("Mete tu nombre");
+var apellidos = prompt("Mete tus apellidos");
+var texto = `<h1>Hola que tal</h1> <h3>Mi nombre es: ${nombre}</h3> <h3>Mis apellidos son: ${apellidos}</h3>`;
+document.write(texto);
