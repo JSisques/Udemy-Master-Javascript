558abbd713b196dce198aa70b1753b1d0d890ed7
Javi Plaza <javierplazasisques@gmail.com>
2021-10-14 11:25:39 +0200

    Seccion 12: Funciones flecha

--- JavaScript/Seccion 12: Funciones y metodos/js/main.js
+++ JavaScript/Seccion 12: Funciones y metodos/js/main.js
@@ -46,3 +46,17 @@ listadoFrutas(...frutas, "Sandia", "Coco", "Pera");
 var pelicula = function(nombre){
     return "La pelicula es: " + nombre;
 }
+
+function sumame(num1, num2, sumaYMuestra, sumaPorDos){
+    var sumar = num1 + num2;
+    sumaYMuestra(sumar);
+    sumaPorDos(sumar);
+    
+    return sumar;
+}
+
+sumame(5, 7, dato => {
+    console.log("La suma es: ", dato);
+}, dato =>{
+    console.log("La suma por dos es: ", (dato*2));
+});
