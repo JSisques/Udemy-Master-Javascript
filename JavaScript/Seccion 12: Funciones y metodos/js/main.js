6f300036f6a895fbc1c06cbfb7792927393d39e4
Javi Plaza <javierplazasisques@gmail.com>
2021-10-14 11:06:20 +0200

    Seccion 12: Parámetros opcionales

--- JavaScript/Seccion 12: Funciones y metodos/js/main.js
+++ JavaScript/Seccion 12: Funciones y metodos/js/main.js
@@ -1,12 +1,25 @@
-function calculadora(num1, num2){
-    console.log("La suma es: " + (num1 + num2));
-    console.log("La resta es: " + (num1 - num2));
-    console.log("La multiplicación es: " + (num1 * num2));
-    console.log("La división es: " + (num1 / num2));
-    console.log("*********************************")
+function calculadora(num1, num2, mostrar = false){
+    if(mostrar){
+        console.log("La suma es: " + (num1 + num2));
+        console.log("La resta es: " + (num1 - num2));
+        console.log("La multiplicación es: " + (num1 * num2));
+        console.log("La división es: " + (num1 / num2));
+        console.log("*********************************");
+    } else{
+        document.write("La suma es: " + (num1 + num2));
+        document.write("La resta es: " + (num1 - num2));
+        document.write("La multiplicación es: " + (num1 * num2));
+        document.write("La división es: " + (num1 / num2));
+        document.write("*********************************");
+    }
 }
 
+calculadora(5,2,true);
+calculadora(5,2,false);
+
+/*
 for (let index = 0; index < 10; index++) {
     var resultado = calculadora(index,8);
     console.log(resultado); 
 }
+*/
