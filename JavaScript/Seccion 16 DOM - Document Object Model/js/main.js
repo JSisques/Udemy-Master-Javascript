914fc70df27027f3f8ee64c8d9bc94a1cd9e97bf
JSisques <javierplazasisques@gmail.com>
2021-10-20 21:58:23 +0200

    Seccion 16: Practicando con el DOM

--- JavaScript/Seccion 16 DOM - Document Object Model/js/main.js
+++ JavaScript/Seccion 16 DOM - Document Object Model/js/main.js
@@ -16,10 +16,17 @@ caja.className = "hola";
 
 console.log(caja);
 */
-var todosLosDivs = document.getElementsByTagName("div");
+var todosLosDivs = document.getElementsByTagName('div');
 var contenidoEnTexto = todosLosDivs[2]
 contenidoEnTexto.innerHTML = "Otro texto para el segundo elemento"
 
 console.log(contenidoEnTexto)
 
 var hr = document.createElement("hr")
+
+//Seleccionar una clase
+var divRojos = document.getElementsByClassName('rojo')
+for (let index = 0; index < divRojos.length; index++) {
+    divRojos[index].style.background = "red";
+    
+}
