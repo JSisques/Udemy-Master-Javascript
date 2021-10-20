8e6a5322a28f03c79e48b2feaa8f6acafda12e12
JSisques <javierplazasisques@gmail.com>
2021-10-20 13:03:39 +0200

    Seccion 13: Arrays básicos

---
+++ JavaScript/Seccion 13 Arrays/js/main.js
@@ -0,0 +1,9 @@
+var numeros = [0,1,2,3,4,5,6,7,8,9]
+var nombres = ["Javi", "Paco", "Poli"]
+
+var datos = [1,"string", 2.5]
+
+var lenguajes = new Array("PHP", "JS", "Java")
+
+console.log(nombres)
+console.log(lenguajes[1])
