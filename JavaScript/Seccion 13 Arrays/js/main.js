100572dfdf936c50cc2544dcc7e30d264d918112
JSisques <javierplazasisques@gmail.com>
2021-10-20 13:09:02 +0200

    Seccion 13: Arrays avanzados

--- JavaScript/Seccion 13 Arrays/js/main.js
+++ JavaScript/Seccion 13 Arrays/js/main.js
@@ -7,3 +7,15 @@ var lenguajes = new Array("PHP", "JS", "Java")
 
 console.log(nombres)
 console.log(lenguajes[1])
+
+//Propiedades array
+console.log(nombres.length)
+var elemento = parseInt(prompt("Que elemento quieres?", 0))
+if(elemento < nombres.length){
+    alert(nombres[elemento])
+}
+
+document.write("<h1>Lenguajes de programación</h1>")
+for (let index = 0; index < lenguajes.length; index++) {
+    document.write("<p>" + lenguajes[index] + "</p>")
+}
