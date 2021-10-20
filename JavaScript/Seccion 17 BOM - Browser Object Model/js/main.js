49969c751eadbb6efc25e2f14ced2b3d1a9e959e
JSisques <javierplazasisques@gmail.com>
2021-10-20 22:08:20 +0200

    Seccion 17 Manipular el navegador y obtener datos con JavaScript

---
+++ JavaScript/Seccion 17 BOM - Browser Object Model/js/main.js
@@ -0,0 +1,15 @@
+//Altura y anchura de la ventana
+console.log(window.innerHeight)
+console.log(window.innerWidth)
+
+//Altura y anchura de la pantalla
+console.log(screen.height)
+console.log(screen.width)
+
+//Ver datos de la URL
+console.log(window.location)
+console.log(window.location.href)
+
+function abrirVentana(url){
+    window.open(url)
+}
